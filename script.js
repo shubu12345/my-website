b7a6e183443895e09a60175c954ab6a52a202bd8
shubu12345 <76595607+shubu12345@users.noreply.github.com>
2021-11-20 13:46:41 +0530

    Add files via upload
    changes

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 var i=0;
-function textChange()
+var txt = [ "a Fresher", "a Web Developer", "a Designer ", "a Front end Developer", "a Indian"];
+var speed = 150;
+function textChange(val)
 {
-    var textChange = document.getElementById("ppo");
-    var text = ["a Web Developer","a Indian","a Designer ","a Fresher"];
-    textChange.innerHTML= text[i];
-    i = (i + 1) % text.length;
-}setInterval(textChange, 3000);
+    const textData = txt[val];
+    if(i<textData.length){
+        document.getElementById("ppo").innerHTML += textData.charAt(i);
+        i++;
+        setTimeout(() =>{
+            textChange(val);
+        }, speed);
+    }else {
+        const last = txt[txt.length - 1]; 
+        if(textData != last){
+        setTimeout(() => {
+            i = 0;
+            document.getElementById("ppo").innerHTML= "";
+            textChange(txt.indexOf(textData) + 1);
+    },speed);
+}else {
+    setTimeout(() => {
+        i = 0;
+        document.getElementById("ppo").innerHTML= "";
+        textChange(0);
+},speed);
+}
+}
+} 
+textChange(0)
